992c202907bec5b3a10695bb787a036289ab03e8
abalmazik <yana.abalmaz@gmail.com>
2018-06-15 14:36:39 +0300

    Create class WebUrl

--- shorten_url/models.py
+++ shorten_url/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class WebUrl(models.Model):
+	url = models.URLField()
+	shot_url = models.CharField(max_length=30)
+	date_create = models.DateTimeField(auto_now=True)
+	visit_count = models.IntegerField(default=0)
+
+
